6516484be9203a529423267bf28ac80d78783b8d
mindcool24 <thezachm24@gmail.com>
2023-11-20 11:46:07 -0700

    Added simple models for starting out

--- _server/core/models.py
+++ _server/core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete.CASCADE)
+    title = models.TextField()
+    content = models.TextField()
+
+class Reply(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete.CASCADE)
+    post = models.ForeignKey(Post, on_delete.CASCADE)
+    replyParent = models.ForeignKey(Reply, on_delete.CASCADE)
+    content = models.TextField()
