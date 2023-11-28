3dad54e3a9572c1c9020984319ef85954e09aaf5
Thomas LeSueur <a02406824@usu.edu>
2023-11-28 11:14:26 -0700

    got the models working

--- _server/core/models.py
+++ _server/core/models.py
@@ -1,14 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.TextField()
     content = models.TextField()
 
 class Reply(models.Model):
     id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete.CASCADE)
-    post = models.ForeignKey(Post, on_delete.CASCADE)
-    replyParent = models.ForeignKey(Reply, on_delete.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    replyParent = models.ForeignKey('self', on_delete=models.CASCADE)
     content = models.TextField()
