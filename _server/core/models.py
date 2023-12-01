b95eae1a85d78779d21b1ce72bdb829502870166
mindcool24 <thezachm24@gmail.com>
2023-11-30 22:04:12 -0700

    Changed models to work better

--- _server/core/models.py
+++ _server/core/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Topic(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+
 class Post(models.Model):
-    id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     title = models.TextField()
     content = models.TextField()
+    parent = models.ForeignKey('self', on_delete=models.CASCADE)
 
-class Reply(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+class Image(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    replyParent = models.ForeignKey('self', on_delete=models.CASCADE)
-    content = models.TextField()
+    file = models.ImageField(upload_to="images")
