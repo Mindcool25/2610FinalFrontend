c62d1b47f64cb33972a5b30f4d403ee3ad52945f
mindcool24 <thezachm24@gmail.com>
2023-11-30 22:32:55 -0700

    Added urls for API

--- _server/core/urls.py
+++ _server/core/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', view=views.index, name="index"),
     #path('post', view=views.post, name="post"),
+    #path('images', view=views.images, name="images"),
+    #path('topic', view=views.topic, name="topic"),
 ]
