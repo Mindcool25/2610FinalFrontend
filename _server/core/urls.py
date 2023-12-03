57819ff192933c9374d0e5be76e56f9310c7cf76
mindcool24 <thezachm24@gmail.com>
2023-12-03 09:32:18 -0700

    Added in routes and basic class structure

--- _server/core/urls.py
+++ _server/core/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', view=views.index, name="index"),
     #path('post', view=views.post, name="post"),
+    #path('post/<int:id>/", views.GetPost.as_view()),
     #path('images', view=views.images, name="images"),
+    #path('images/<int:id>/", views.GetImage.as_view()),
     #path('topic', view=views.topic, name="topic"),
+    #path('topic/<int:id>/', view=views.GetTopic.as_view()),
 ]
--- _server/core/views.py
+++ _server/core/views.py
@@ -1,5 +1,7 @@
+from django.views import View
 from django.shortcuts import render
 from django.conf  import settings
+from django.http import HttpResponse
 import json
 import os
 from django.contrib.auth.decorators import login_required
@@ -21,3 +23,51 @@ def index(req):
         "css_file": "" if settings.DEBUG else MANIFEST["src/main.ts"]["css"][0]
     }
     return render(req, "core/index.html", context)
+
+class Post(View):
+    # Return error, get not allowed
+    def get(self, request):
+        return
+    # Create a new post
+    def post(self, request):
+        return
+
+class GetPost(View):
+    # Get given post
+    def get(self, request):
+        return
+    # Edit given post (If correct user)
+    def post(self, request):
+        return
+
+class GetImage(View):
+    # Get given image 
+    def get(self, request):
+        return
+    # Remove given image (if logged in)
+    def post(self, request):
+        return
+
+class Topic(View):
+    # Return all topics
+    def get(self, request):
+        return
+    # Create a new topic
+    def post(self, request):
+        return
+
+class GetTopic(View):
+    # Return given topic
+    def get(self, request):
+        return
+    # Edit given topic (If logged in)
+    def post(self, request):
+        return
+
+class Images(View):
+    # Get an image
+    def get(self, request):
+        return
+    # Save an image
+    def post(self, request):
+        return
