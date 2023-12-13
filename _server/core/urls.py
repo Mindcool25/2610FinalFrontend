8336fb9975d1a4ac68b8259c671e641f2032801b
mindcool24 <thezachm24@gmail.com>
2023-12-13 12:53:45 -0700

    changed URL

--- _server/core/urls.py
+++ _server/core/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', view=views.index, name="index"),
     path('post', view=views.Post.as_view(), name="post"),
-    path('post/<int:id>/', views.GetPost.as_view()),
+    path('getpost/<int:id>/', views.GetPost.as_view()),
     path('images', view=views.Images.as_view(), name="images"),
     path('images/<int:id>/', views.GetImage.as_view()),
     path('topic', view=views.Topic.as_view(), name="topic"),
