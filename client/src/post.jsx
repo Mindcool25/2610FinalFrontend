9258c3d31d4df6f4f5849bfbc2f4c27d6718fa13
Thomas LeSueur <a02406824@usu.edu>
2023-12-13 11:46:42 -0700

    things

---
+++ client/src/post.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react'
+
+function wholePost(props){
+    const [posts, setPosts] = useState([])
+
+    async function getPost(id) {
+        const res = await fetch("/post/1", {
+          credentials: "same-origin", // include cookies!
+        });
+    
+        if (res.ok) {
+          const body = await res.json
+          return body.json();
+        } else {
+          // handle logout failed!
+        }
+      }
+    useEffect ( () => {
+        const newPosts = getPost(props.id)
+        setPosts([...posts, ...newPosts]);
+    }, []);
+
+    function Data(props){
+        return (
+            <div className="post">
+                <h2 className="postTitle">{props.post["title"]}</h2>
+                <h3 className="postUser">{props.post["user"]}</h3>
+                <p className="postContent">{props.post["content"]}</p>
+            </div>
+        )
+    }
+    
+    const r = posts.map((post) =>
+    <Data post={post} key={post[id]}/>
+    );
+    return(
+        <div>
+            {r}
+        </div>
+    )
+
+}
