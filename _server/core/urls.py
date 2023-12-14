e28ee246849aa56c09d88b83f6e1235fd6301dbe
Thomas LeSueur <a02406824@usu.edu>
2023-12-14 14:53:32 -0700

    url was bad

--- _server/core/urls.py
+++ _server/core/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('newpost', view=views.new_post, name="new post"),
     path('getpost/<int:id>/', views.GetPost.as_view()),
     path('images', view=views.new_image, name="images"),
-    path('images/<int:id>/', views.GetImage.as_view()),
+    path('images/<int:id>/', view=views.get_image),
     path('newtopic', view=views.new_topic, name="new topic"),
     path('gettopic/<int:id>/', view=views.get_topic, name="get topic"),
     path('gettopics', view=views.get_topics),
